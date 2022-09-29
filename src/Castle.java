bcb7afd58e1a983b5f8a333c37a19980a86bf78c
ybbro <ybbroom@gmail.com>
2022-09-29 23:09:48 +0300

    create class for every tool

---
+++ src/Castle.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class Castle extends Tool {
+
+    public Castle(int inxX, int inxY, Color color) {
+        super(inxX, inxY, color);
+    }
+
+    public boolean movable(int toX , int toY)
+    {
+        return true;
+    }
+    public void Move(int toX,int toY)
+    {
+
+    }
+
+
+}
