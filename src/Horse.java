b595421455f7d6a38c81274cb718172fff4088c4
Admin <neriadaviddahan@gmail.com>
2022-09-29 22:11:20 +0200

    update class of horse

--- src/Horse.java
+++ src/Horse.java
@@ -1,3 +1,20 @@
-public class Horse {
+import java.awt.*;
 
+public class Horse extends Tool{
+
+    public Horse(int inxX, int inxY, Color color) {
+        super(inxX, inxY, color);
+    }
+    public boolean ableToMove(int x,int y)
+    {
+        if (this.inxX-x != 0)
+            return((Math.abs(this.inxX-x) < 3 && Math.abs(this.inxY-y) < 3)&&(Math.abs((this.inxY-y)/(this.inxX-x)) == 2 || Math.abs((this.inxY-y)/(this.inxX-x)) == 0.5));
+        else
+            return false;
+    }
+    public void Move(int x,int y)
+    {
+        this.inxX = x;
+        this.inxY = y;
+    }
 }
