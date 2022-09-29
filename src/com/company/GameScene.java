595347bf06766dd6d10079ca1b90ba5102517809
yair <yair.yaad.ben.shimol@gmail.com>
2022-09-30 00:09:47 +0300

    add print

--- src/com/company/GameScene.java
+++ src/com/company/GameScene.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class GameScene extends JPanel {
 
@@ -8,14 +10,28 @@ public class GameScene extends JPanel {
 
     public static Tool[][] tools = new Tool[8][8];
 
-    GameScene(int height , int width){
+    boolean flag;
 
-        //for (int i = 0; i < 8; i++) {
-        //    for (int j = 0; j < 8; j++) {
-        //        Squares[i][i] = new JButton();
-        //        Squares[i][i].
-        //    }
-        //}
+    GameScene(int height , int width){
+        this.setBounds(0,0,width , height);
+        this.setLayout(new GridLayout(8,8));
+        flag = false;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Squares[i][i] = new JButton();
+                Squares[i][i].addActionListener((ActionListener) this);
+                Squares[i][i].setFocusable(false);
+                if(!flag){
+                    Squares[i][i].setBackground(new Color(200,100,10));
+                    flag = true;
+                }else {
+                    Squares[i][i].setBackground(new Color(100, 50, 10));
+                    flag = false;
+                }
+            }
+        }
+
 
     }
 }
